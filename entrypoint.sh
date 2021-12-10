282afbe9b3d9d8876e8a021575452fd14831380a
Chris Castle <chris@render.com>
2021-12-10 17:25:35 -0500

    Explicitly set web console as publicly exposed port, but make it easily configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # wrapper for docker entrypoint that takes into account the PORT env var
 
-exec docker-entrypoint.sh server --console-address ":${PORT:-9001}" /data
+exec docker-entrypoint.sh server --console-address ":10000" --address ":9000" /data
