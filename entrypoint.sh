4403ca0e1277841f9b9eb31f453ae80b09bbe44a
Anurag Goel <anurag@render.com>
2022-03-06 18:50:42 -0800

    Use MinIO env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # wrapper for docker entrypoint that takes into account the PORT env var
-
-exec docker-entrypoint.sh server --console-address ":10000" --address ":9000" /data
+export MINIO_BROWSER_REDIRECT_URL="$RENDER_EXTERNAL_URL"
+exec docker-entrypoint.sh server /data
